9fb9dd9f60e2ef5f4da33a8788554398cbc61647
FLY <2401951498@qq.com>
2023-07-09 09:13:33 +0800

    Apifox

---
+++ slideshow.js
@@ -0,0 +1,57 @@
+let left = document.querySelector(".left")
+let right = document.querySelector(".right")
+let small = document.querySelectorAll(".small")
+let images = document.querySelector(".big-img")
+let smallImg = document.querySelector(".small-img")
+let index = 0
+let time
+function position() {
+    images.style.left = (index * -100) + "%"
+    smallImg.style.left = (index * -9) + "%"
+}
+function add() {
+    if (index >= small.length - 1) {
+        index = 0
+
+    }
+    else {
+        index++
+    }
+}
+function desc() {
+    if (index < 1) {
+        index = small.length - 1
+    }
+    else {
+        index--
+    }
+}
+function timer() {
+    time = setInterval(() => {
+        index++;
+        desc()
+        add()
+        position()
+    }, 3000)
+}
+left.addEventListener("click", () => {
+    desc()
+    position()
+    clearInterval(time)
+    timer()
+})
+right.addEventListener("click", () => {
+    add()
+    position()
+    clearInterval(time)
+    timer()
+})
+for (let i = 0; i < small.length; i++) {
+    small[i].addEventListener("click", () => {
+        index = i
+        position()
+        clearInterval(time)
+        timer()
+    })
+}
+timer()
